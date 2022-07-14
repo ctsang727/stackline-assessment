451a19501d3aefc738d4819ecadcff7c393791d4
Chris Tsang <christopher.tsang@uconn.edu>
2022-07-14 12:48:19 -0400

    done

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getDataThunk } from '../../store/chart'
 import './chart.css'
@@ -6,7 +6,12 @@ import './chart.css'
 function Chart() {
     const testState = useSelector(state => state.data)
     const test2 = Object.values(testState)
+    const preview = test2[0]?.sales.slice(0, 15)
+    //console.log('pre', preview)
+
+    const [showMore, setShowMore] = useState(false)
     //console.log('2222', test2[0]?.sales)
+    console.log(showMore)
 
     //console.log('state', testState)
 
@@ -16,6 +21,10 @@ function Chart() {
         dispatch(getDataThunk())
     }, [dispatch])
 
+    const showAll = () => {
+        setShowMore(!showMore)
+    }
+
 
 
 
@@ -23,36 +32,87 @@ function Chart() {
     return (
 
         <div id='chart'>
-            <div id='week-ending'>
-                <h3>WEEK ENDING</h3>
-                {test2[0]?.sales.map(sale => {
-                    return <li>{sale.weekEnding}</li>
-                })}
-            </div>
-            <div id='retail-sales'>
-                <h3>Retail Sales</h3>
-                {test2[0]?.sales.map(sale => {
-                    return <li>{sale.retailSales}</li>
-                })}
-            </div>
-            <div id='wholesale-sales'>
-                <h3>Wholesale Sales</h3>
-                {test2[0]?.sales.map(sale => {
-                    return <li>{sale.wholesaleSales}</li>
-                })}
-            </div>
-            <div id='units-sold'>
-                <h3>Units Sold</h3>
-                {test2[0]?.sales.map(sale => {
-                    return <li>{sale.unitsSold}</li>
-                })}
-            </div>
-            <div id='retailer-margin'>
-                <h3>Retailer Margin</h3>
-                {test2[0]?.sales.map(sale => {
-                    return <li>{sale.retailerMargin}</li>
-                })}
-            </div>
+            {!showMore &&
+                <>
+                    <div id='inner-chart'>
+                        <div id='week-ending'>
+                            <h3>WEEK ENDING</h3>
+                            {preview?.map(sale => {
+                                return <li>{sale.weekEnding}</li>
+                            })}
+                        </div>
+                        <div id='retail-sales'>
+                            <h3>Retail Sales</h3>
+                            {preview?.map(sale => {
+                                return <li>${sale.retailSales}</li>
+                            })}
+                        </div>
+                        <div id='wholesale-sales'>
+                            <h3>Wholesale Sales</h3>
+                            {preview?.map(sale => {
+                                return <li>${sale.wholesaleSales}</li>
+                            })}
+                        </div>
+                        <div id='units-sold'>
+                            <h3>Units Sold</h3>
+                            {preview?.map(sale => {
+                                return <li>{sale.unitsSold}</li>
+                            })}
+                        </div>
+                        <div id='retailer-margin'>
+                            <h3>Retailer Margin</h3>
+                            {preview?.map(sale => {
+                                return <li>${sale.retailerMargin}</li>
+                            })}
+
+                        </div>
+
+                    </div>
+                    <div id='showButton' onClick={showAll}>
+                        Show All &#9660;
+                    </div>
+                </>}
+            {/* show all  */}
+            {showMore &&
+                <>
+                    <div id='inner-chart'>
+                        <div id='week-ending'>
+                            <h3>WEEK ENDING</h3>
+                            {test2[0]?.sales.map(sale => {
+                                return <li>{sale.weekEnding}</li>
+                            })}
+                        </div>
+                        <div id='retail-sales'>
+                            <h3>Retail Sales</h3>
+                            {test2[0]?.sales.map(sale => {
+                                return <li>${sale.retailSales}</li>
+                            })}
+                        </div>
+                        <div id='wholesale-sales'>
+                            <h3>Wholesale Sales</h3>
+                            {test2[0]?.sales.map(sale => {
+                                return <li>${sale.wholesaleSales}</li>
+                            })}
+                        </div>
+                        <div id='units-sold'>
+                            <h3>Units Sold</h3>
+                            {test2[0]?.sales.map(sale => {
+                                return <li>{sale.unitsSold}</li>
+                            })}
+                        </div>
+                        <div id='retailer-margin'>
+                            <h3>Retailer Margin</h3>
+                            {test2[0]?.sales.map(sale => {
+                                return <li>${sale.retailerMargin}</li>
+                            })}
+                        </div>
+                    </div>
+                    <div id='showLessButton' onClick={showAll}>
+                        Show less &#9650;
+                    </div>
+                </>}
+
+
         </div>
 
 
